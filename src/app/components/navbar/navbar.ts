2fa7ee3345dce45cf31ecf6041239e83145431d5
phalla009 <hphalla3@gmail.com>
2026-02-18 18:04:35 +0700

    edit 4

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -17,15 +17,29 @@ export class Navbar {
     event.preventDefault();
     this.isCategoryOpen = !this.isCategoryOpen;
   }
+  private toggleBodyScroll() {
+    if (this.isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+
+    this.toggleBodyScroll();
+  }
   closeMenu() {
     this.isOpen = false;
     this.isCategoryOpen = false;
+    document.body.style.overflow = 'auto';
   }
   @HostListener('document:click', ['$event'])
   onClickOutside(event: Event) {
     const clickedInside = this.elRef.nativeElement.contains(event.target);
     if (!clickedInside) {
       this.isOpen = false;
+      document.body.style.overflow = 'auto';
     }
   }
 }
